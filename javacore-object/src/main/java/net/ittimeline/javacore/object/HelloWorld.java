ef0a515b26fa91c5a8aaff02c3938e56678831a1
wangquan <1126909120@qq.com>
2018-10-21 15:12:45 +0800

    Initial commit

---
+++ javacore-object/src/main/java/net/ittimeline/javacore/object/HelloWorld.java
@@ -0,0 +1,12 @@
+package net.ittimeline.javacore.object;
+
+/**
+ * 我的第一个Java程序
+ *
+ * @author wangquan 1126909120@qq.com
+ * @create 2018/10/2115:03
+ * @since JDK11
+ */
+public class HelloWorld {
+
+}
