92f17b4f59ed9cdf932c30985e1c93046e8b9322
wangquan <1126909120@qq.com>
2018-10-21 15:45:20 +0800

    GitHub代码提交测试

---
+++ javacore-object/src/main/java/net/ittimeline/javacore/object/Welcome.java
@@ -0,0 +1,15 @@
+package net.ittimeline.javacore.object;
+
+/**
+ * Welcome
+ *
+ * @author wangquan 1126909120@qq.com
+ * @create 2018/10/2115:26
+ * @since JDK11
+ */
+public class Welcome {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome java//");
+    }
+}
